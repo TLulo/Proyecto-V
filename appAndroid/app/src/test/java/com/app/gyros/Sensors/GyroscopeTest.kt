d7ff135621d41a3352d8177254cdc8cd42814ba5
lalo <luciano.tula@mi.unc.edu.ar>
2025-07-09 14:08:17 -0300

    Se anaden los test de las features incorporadas

---
+++ appAndroid/app/src/test/java/com/app/gyros/Sensors/GyroscopeTest.kt
@@ -0,0 +1,37 @@
+package com.app.gyros.Sensors
+
+import android.hardware.Sensor
+import android.hardware.SensorManager
+import io.mockk.every
+import io.mockk.mockk
+import org.junit.Test
+import org.junit.Assert.*
+
+class GyroscopeTest {
+    @Test
+    fun `TestDetect return true when sensor exist`() {
+        val mockSensorManager = mockk<SensorManager>()
+        val mockSensor = mockk<Sensor>()
+
+        every { mockSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) } returns mockSensor
+
+        val gyroscope = Gyroscope(mockSensorManager)
+
+        val result = gyroscope.detectSensor()
+
+        assertTrue(result)
+    }
+
+    @Test
+    fun `TestDetect return false when sensor does not exist`() {
+        val mockSensorManager = mockk<SensorManager>()
+
+        every { mockSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE) } returns null
+
+        val gyroscope = Gyroscope(mockSensorManager)
+
+        val result = gyroscope.detectSensor()
+
+        assertFalse(result)
+    }
+}
