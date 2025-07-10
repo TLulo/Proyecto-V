80049fc9a14260e25e2ba4f37068c2a7746c6544
lalo <luciano.tula@mi.unc.edu.ar>
2025-07-09 22:36:33 -0300

    Se anade la lectura e impresion del accelerometro

--- appAndroid/app/src/main/java/com/app/gyros/MainActivity.kt
+++ appAndroid/app/src/main/java/com/app/gyros/MainActivity.kt
@@ -5,53 +5,68 @@ import android.hardware.SensorManager
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.app.gyros.Sensors.Accelerometer
 import com.app.gyros.Sensors.Gyroscope
+import com.app.gyros.Sensors.Utils.SensorViewModel
 import com.app.gyros.ui.theme.GyrosTheme
 
 class MainActivity : ComponentActivity() {
+    private lateinit var accelerometer: Accelerometer
+    private lateinit var gyroscope: Gyroscope
+    private val sensorViewModel : SensorViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        accelerometer = Accelerometer(sensorManager)
+        accelerometer.setViewModel(sensorViewModel)
+
         setContent {
             GyrosTheme {
-                Surface(
+                Surface (
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
-                ) {
+                    color = Color.White,
+                ){
                 }
-                Detected(this)
+                SensorScreen(sensorViewModel)
             }
         }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        accelerometer.start()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        accelerometer.stop()
     }
 }
 
 @Composable
-fun Detected(context: Context, modifier: Modifier = Modifier){
-    val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
-    val accelerometer = Accelerometer(sensorManager)
-    val gyroscope = Gyroscope(sensorManager)
+fun SensorScreen(viewModel: SensorViewModel) {
+    val (x, y, z) = viewModel.SensorValues
 
-    Surface() {
-        Box(Modifier.fillMaxSize(),contentAlignment = Alignment.Center){
-            Text(
-                text = (if (accelerometer.detectSensor()) "The user Has a Accelerometer"
-                            else "The user Hasn't a Accelerometer")
-                        + "\n" +
-                        (if (gyroscope.detectSensor()) "The user Has a Gyroscope"
-                            else "The user Hasn't a Gyroscope"),
-                modifier = modifier.padding(50.dp)
-            )
-        }
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text("Sensor:")
+        Text("X: $x")
+        Text("Y: $y")
+        Text("Z: $z")
     }
 }
--- appAndroid/app/src/main/java/com/app/gyros/Sensors/Accelerometer.kt
+++ appAndroid/app/src/main/java/com/app/gyros/Sensors/Accelerometer.kt
@@ -1,13 +1,46 @@
 package com.app.gyros.Sensors
 
 import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
+import com.app.gyros.Sensors.Utils.SensorViewModel
 
 
-class Accelerometer(private val sensorManager: SensorManager) : AbstractSensor(){
+class Accelerometer(private val sensorManager: SensorManager) : AbstractSensor(), SensorEventListener{
+
+    private lateinit var viewModel : SensorViewModel
+
+        public fun setViewModel(viewModel: SensorViewModel) {
+            this.viewModel = viewModel
+        }
 
     public override fun detectSensor(): Boolean {
         return (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null)
+    }
+
+    fun start(){
+        var sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+        if (sensor != null)
+            sensorManager.registerListener(this,sensor, SensorManager.SENSOR_DELAY_GAME)
+    }
+
+    fun stop(){
+        sensorManager.unregisterListener(this)
+    }
+
+    override fun onSensorChanged(event: SensorEvent?){
+        if (event != null){
+            val x = event.values[0]
+            val y = event.values[1]
+            val z = event.values[2]
+
+            viewModel.updateValues(x,y,z)
+        }
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+
     }
 
 }
