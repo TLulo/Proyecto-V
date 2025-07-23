9a44129c8648a15e143f1987db823e2164ecca8e
lalo <luciano.tula@mi.unc.edu.ar>
2025-07-23 20:11:26 -0300

    Optimize Imports con androidStudio

--- appAndroid/app/src/main/java/com/app/gyros/MainActivity.kt
+++ appAndroid/app/src/main/java/com/app/gyros/MainActivity.kt
@@ -31,7 +31,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.fragment.app.DialogFragment
 import com.app.gyros.Sensors.AbstractSensor
 import com.app.gyros.Sensors.Accelerometer
 import com.app.gyros.Sensors.Gyroscope
